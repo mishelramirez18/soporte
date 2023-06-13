35ed2c946d55f4a33b014066777b5465de3857bc
mishelramirez18 <128616625+mishelramirez18@users.noreply.github.com>
2023-06-13 10:58:52 -0600

    Add files via upload

---
+++ parcial.py
@@ -0,0 +1,15 @@
+nombre= int (input("ingrese el nombre"))
+edad = int (input("ingrese la edad"))
+
+if edad %8=<0:
+print("si es par puede entrar al discoteca")
+
+elif edad = 1,3,5,7,9,11,13,
+print("si no es par puede entrar entrar al comedor")
+
+
+
+
+
+
+
