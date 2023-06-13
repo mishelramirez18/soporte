19aecba07b0b6049fae0a12191c0a6e887447012
Karla## <karla@gmail.com>
2023-06-13 10:57:34 -0600

    examen

---
+++ examen.py
@@ -0,0 +1,20 @@
+ normalizacion
+(logro entender ciertas normalizaciones pero siempre encuentro confusion
+sobre en ciertos atributos ya sea en hacer lo de pk o hacer las fk o de poner cada atributo en diferente tabla
+cuando intento ver la loguca de las normalizaciones vuelvo a confundirme en como poner cada dato e diferente tabla)
+
+ acces
+(hacer la relacion de algunas listas logro mas o menos de entender lo que se me dificulta es ver lo de formulario y tambien otro tipos
+suelo hacer los ejercicios muy despacio y me distraigo en el programa)
+
+ git bash 
+ (se me dificulta los comandos y me cuesta tratar de subirlo a github algun commit a veces me cuesta hacerlo o cambiar de rama 
+ o hacer algun origin y crear una rama)
+
+  git hub
+(a veces no entiendo donde hacer un repositorio o me cuesta subirlo o se me dificulta crear el enlace)
+
+ djnago
+(no logro entender cada paso o como isntalarlo o como se puede hacer en especifico como hacer que aparezca en chrome)
+
+
